86c350f7b2bd8cbe1acab96404d8515d6fb34551
Saiteja802 <saiteja802@gmail.com>
2024-07-16 12:56:07 +1200

    Update timehistory_parallel.py

--- timehistory_parallel.py
+++ timehistory_parallel.py
@@ -17,14 +17,14 @@ def RHA(intensity,gmno,saveloc,GMloc):
     timeSeries('Path',3,'-dt',dt,'-values',*gacc)   # time series
     pattern('UniformExcitation', 3, 1,'-accel', 3)  # acc loading pattern 
     'Acceleration response recorder'
-    recorder('Node', '-file', saveloc+"/displacement."+str(gmno)+".out", '-time', '-node'  ,*cntrl_nodes, '-dof',1,'disp')  # dispalcement recorder. Replace with the node numbers and dof of interest based on your model
-    recorder('Node', '-file', saveloc+"/acceleration."+str(gmno)+".out",'-timeSeries',3,'-time','-node',*cntrl_nodes,'-dof',1,'accel') # acceleration recorder. Replace with the node numbers and dof of interest based on your model
+    recorder('Node', '-file', saveloc+"/displacement."+str(gmno)+".out", '-time', '-node'  ,*cntrl_nodes, '-dof',1,'disp')  # dispalcement recorder at the control nodes
+    recorder('Node', '-file', saveloc+"/acceleration."+str(gmno)+".out",'-timeSeries',3,'-time','-node',*cntrl_nodes,'-dof',1,'accel') # total acceleration recorder at the control nodes
     wipeAnalysis()		  # clear previously-define analysis parameters    
     # analysis parameters
     system('BandGeneral') # how to store and solve the system of equations in the analysis
     constraints('Plain')  # how it handles boundary conditions
-    numberer('Plain')     # Use RCM if the model is complex. This scheme will renumber dof's to minimize stiffness matrix band-width
-    algorithm('Linear')	  # use Linear algorithm for linear analysis. DO NOT USE LINEAR ALGORITHM FOR NONLINEAR PROBLEMS
+    numberer('Plain')     # Use RCM if the model is complex. RCM scheme will renumber dof to minimize stiffness matrix band-width
+    algorithm('Linear')	  # Use Linear algorithm as this is a linear elastic analysis.
     test('NormUnbalance', 1.0e-3, 500, 0, 0)
     integrator('Newmark',0.5,0.25) # using the newmark's average acceleration scheme (implicit scheme)
     analysis('Transient') # define type of analysis: time-dependent
@@ -32,5 +32,3 @@ def RHA(intensity,gmno,saveloc,GMloc):
     for i in range(len(gacc)):
         analyze(1, dt)	  # Run the analysis at a constant time step dt
     wipe()
-  
-# RHA(1,2,'P:/0_parallel_computing_MSA/'+ 'MSA_results/level_'+str(1),'P:/0_parallel_computing_MSA/'+ 'GMS/level_'+str(1))
